741375dbc23126f71daa79ed50a25d1da137ec08
kcaisley <kcaisley@uni-bonn.de>
2023-09-06 20:19:22 +0200

    Update TCAD startup script

--- startup_scripts/sentaurus.sh
+++ startup_scripts/sentaurus.sh
@@ -1,10 +1,13 @@
-#setup TCAD stuffs
+# setup TCAD environment
+# usage `source sentaurus.sh
 export LM_LICENSE_FILE=8000@faust02.physik.uni-bonn.de
-export STROOT=/tools/synopsys/installs/sentaurus/R_2020.09-SP1
-export PATH=$STROOT/bin:$PATH
-export STDB=/users/kcaisley/tcad
+export STROOT=/tools/synopsys/2022-23/RHELx86/SENTAURUS_2022.12
+export PATH="${PATH}:${STROOT}/bin"
+export STRELEASE=current
+export STDB="${HOME}/tcad"
 
-alias ll='ls -l'
-alias la='ls -la'
+export DMW_RSH_PATH=/usr/bin/ssh
+# swb, svisual; snmesh, sde, tdx, etc...
 
-echo set up dependencies
+echo 'Sentarus TCAD tools are available'
+echo 'swb, svisual; snmesh, sde, tdx, etc...'
