66a2ee6acfa5189e2dfbcc67d9a195412c6d59bf
kcaisley <kcaisley@uni-bonn.de>
2024-05-21 21:18:56 +0200

    cleanup workstation script to use host lists

--- compare_workstations.sh
+++ compare_workstations.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-ssh_list=(  asiclab@asiclab000.physik.uni-bonn.de \
+host_list=( asiclab@asiclab000.physik.uni-bonn.de \
             asiclab@asiclab001.physik.uni-bonn.de \
             asiclab@asiclab002.physik.uni-bonn.de \
             asiclab@asiclab003.physik.uni-bonn.de \
             asiclab@asiclab004.physik.uni-bonn.de \
             asiclab@asiclab006.physik.uni-bonn.de \
             asiclab@asiclab007.physik.uni-bonn.de \
-	    asiclab@asiclab008.physik.uni-bonn.de \
+            asiclab@asiclab008.physik.uni-bonn.de \
             asiclab@asiclab011.physik.uni-bonn.de \
    )
 
-#for host in "${ssh_list[@]}"; do ssh $host "hostname"; done
+# Count number of hosts
+host_count=${#host_list[@]}
+# echo "Comparing packages on $host_count workstations..."
 
-packages=$(for host in "${ssh_list[@]}"; do
+# For host in "${host_list[@]}"; do ssh $host "hostname"; done
+packages=$(for host in "${host_list[@]}"; do
     ssh $host "rpm -qa"
 done)
 
-echo $packages | tr ' ' '\n' | sort | uniq -c | awk '$1 != 9'
+# Replace all spaces with newlines, group packages and count them. Then report those that aren't on all machines. 
+echo $packages | tr ' ' '\n' | sort | uniq -c | awk -v host_count=$host_count '$1 != host_count'
