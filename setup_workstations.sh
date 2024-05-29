396f5c0d91e98a4abe934c0666c794eadc61bd5f
kcaisley <kcaisley@uni-bonn.de>
2024-05-29 14:03:50 +0200

    iverilog install

--- setup_workstations.sh
+++ setup_workstations.sh
@@ -15,9 +15,11 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf install -y code
 
+#Packages needed for Ramon/Basil:
+
 # Build requirements for iverilog
 sudo dnf install -y autoconf gperf make gcc g++ bison flex
-
+# also need to git clone and installed 
 
 # For ngspice
 sudo dnf install -y libXaw-devel
